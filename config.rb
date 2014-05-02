df7a4d70ce57900b7d2c899f181c76644feb1dc6
Stefano Verna <s.verna@cantierecreativo.net>
2014-05-02 18:11:04 +0200

    .flag, .form, .pill, .table

--- config.rb
+++ config.rb
@@ -1,9 +1,14 @@
 activate :relative_assets
+activate :gzip
 
 set :relative_links, true
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
+
+compass_config do |config|
+  config.output_style = :compressed
+end
 
 activate :deploy do |deploy|
   deploy.method = :git
