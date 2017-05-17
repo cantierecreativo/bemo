1ea3341391d9ebdfd29e5c34c23eba9321b4c55d
Matteo Manzo <m.manzo@cantierecreativo.net>
2017-05-17 17:19:41 +0200

    mobile menu and navbar

--- middleman/source/javascripts/index.js
+++ middleman/source/javascripts/index.js
@@ -1,1 +1,9 @@
 // This is where it all goes :)
+
+$(document).ready(function() {
+  $('.toggle-menu').on('click touchstart',  function(e) {
+    e.preventDefault();
+    $('.menu').toggleClass('is-visible');
+    $('.content__overflow').toggleClass('hidden');
+  });
+});
